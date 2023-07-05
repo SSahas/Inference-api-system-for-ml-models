171f644328c863fb4985bef60425ee8da019b6cc
Sahas <82393353+SSahas@users.noreply.github.com>
2023-07-05 22:26:07 +0530

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,32 @@
+from urllib.parse import urljoin
+
+import requests
+
+
+ENDPOINT_URL = "https://intern-sahas-intern-sriram-8000.demo1.truefoundry.com/predict/"
+
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+}
+response = requests.post(
+    urljoin(ENDPOINT_URL, f'predict'),
+    
+    json={
+            "hf_pipeline": "text-generation",
+            "model_deployed_url": "https://text-gen-intern-sriram.demo1.truefoundry.com/v2/models/text-gen/infer/simple",
+
+            "inputs": "Hello, how are you today?",
+            "parameters":{"max_new_tokens": 100,
+                        "temperature": 0.7,
+                        "top_k": 5,
+                        "top_p": 0.9,
+                        "return_full_text": True},
+    }
+
+    
+)
+
+result = response.json()
+
+print(result)
